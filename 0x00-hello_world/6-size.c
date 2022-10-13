2cd867481cd7d7719cf912bc276b83fbce286058
natureThecoder <basilosinachi1@gmail.com>
2022-10-13 16:38:14 -0700

    updating my 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,14 +6,17 @@
  */
 int main(void)
 {
-	int a;
-	float b;
-	double c;
-	char d;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	
 
-	printf("Size of int: %ld bytes \n",sizeof(a));
-	printf("Size of float: %ld bytes \n",sizeof(b));
-	printf("Size of double: %ld bytes \n",sizeof(c));
-	printf("Size of char: %ld bytes \n",sizeof(d));
+	printf("Size of char: %d bytes \n", sizeof(a));
+	printf("Size of int: %d bytes \n", sizeof(b));
+	printf("Size of long int: %d bytes \n", sizeof(c));
+	printf("Size of long long int: %d bytes \n", sizeof(d));
+	printf("Size of float: %d bytes \n", sizeof(e));
 	return (0);
 }
