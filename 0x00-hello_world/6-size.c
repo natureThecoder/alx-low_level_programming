67bb0743fe877772394803b6fa835d6d20657101
natureThecoder <basilosinachi1@gmail.com>
2022-10-13 18:20:00 -0700

    task work

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
