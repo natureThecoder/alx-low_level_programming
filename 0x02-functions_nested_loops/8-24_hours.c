75a67a1a04142cb8253a5bc5f55eb6c5bfd7ce89
natureThecoder <basilosinachi1@gmail.com>
2022-10-19 13:15:32 -0700

    updating task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,16 +6,15 @@ void jack_bauer(void)
 {
 	int h, m;
 
-	for (h = 0; h <= 23; h++)
+	for (h = 0; h < 24; h++)
 	{
-		for (m = 0; m <= 59; m++)
+		for (m = 0; m < 60; m++)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
 			_puychar(':');
 			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
-
 			_putchar('\n');
 		}
 	}
