ff0259f27550c6647423bb29999f7e80839e5618
natureThecoder <basilosinachi1@gmail.com>
2022-10-21 09:37:32 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int m = 0;
+
+	while (m < 10)
+		_putchar(m++ + '0');
+
+	_putchar('\n')
+}
