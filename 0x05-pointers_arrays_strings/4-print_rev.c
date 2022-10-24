6f66087672e64c0350ee377fdb7d9caa0b4a0a23
natureThecoder <basilosinachi1@gmail.com>
2022-10-24 11:43:23 -0700

    task4 update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,13 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, i = 0;
+	int i = 0;
 
-	while (s[i++])
-		len++;
-	for (i = len - 1; i >= 0; 1--)
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
 		_putchar(s[i]);
+
 	_putchar('\n');
+}
 }
