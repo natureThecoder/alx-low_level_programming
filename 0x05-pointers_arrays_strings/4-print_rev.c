1ce78ea95bbd1d27d25be28252453ba1e33b6e9d
natureThecoder <basilosinachi1@gmail.com>
2022-10-24 11:34:56 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be reversed
+ */
+void print_rev(char *s)
+{
+	int len = 0, i = 0;
+
+	while (s[i++])
+		len++;
+	for (i = len - 1; i >= 0; 1--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
