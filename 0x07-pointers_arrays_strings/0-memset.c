da7eb83b787e14f49ef3d6e3d1a1c2676084ec54
natureThecoder <basilosinachi1@gmail.com>
2022-10-31 07:36:15 -0700

    solution to task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,6 +9,13 @@
  * Return: a string
  */
 char *_memset(char *s, char b, unsigned int n)
-}
-	return (memset(s, b, n));
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
 }
