c63f01d4f948f8091b3e3482e83641e99be46b20
natureThecoder <basilosinachi1@gmail.com>
2022-10-31 07:14:18 -0700

    solution to task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * _memset - this is memory function
+ * _memset - this memory set function
  * @s: string
  * @b: a character
  * @n: an integer
@@ -8,10 +9,6 @@
  * Return: a string
  */
 char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int = a;
-
-	for (a = 0; a < n; a++)
-		s[a] = b;
-	return (s);
+}
+	return (memset(s, b, n);
 }
