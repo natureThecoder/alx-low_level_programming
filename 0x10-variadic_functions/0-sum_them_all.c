7e08a28e8465ec0905b4f383a6f0851e3b168594
natureThecoder <basilosinachi1@gmail.com>
2022-11-17 13:54:24 -0800

    task0 update

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,6 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 /**
  * sum_them_all - Entry Point
  * @n: variadic inputs
@@ -7,14 +8,19 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list valist;
 	unsigned int i, sum = 0;
-	va_list arguments;
 
-	va_start(arguments, n);
+	if (n == 0)
+		return (0);
+
+	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
-		sum = sum + va_arg(arguments, int);
+	{
+		sum += va_arg(valist, const unsigned int);
+	}
+	va_end(valist);
 
-	va_end(arguments);
 	return (sum);
 }
